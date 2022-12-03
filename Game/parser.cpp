a15da61f8377baf39ac64fdc784f1eef4891a814
alonz9911 <alons9911@gmail.com>
2022-12-03 23:47:21 +0200

    Add input parsing

---
+++ Game/parser.cpp
@@ -0,0 +1,158 @@
+#include <fstream>
+#include "parser.h"
+#include <iostream>
+using namespace std;
+
+static sceneDesription parseInputFile(string fileName)
+{
+    ifstream infile(fileName);
+
+    int n1, n2;
+    float x1, x2, x3, x4;
+    char op, c;
+
+    if (!(infile >> n1 >> n2))
+    { /* error, could not read first line! Abort. */
+    }
+
+    sceneDesription scene;
+
+    while (infile >> op >> x1 >> x2 >> x3 >> x4 >> c)
+    {
+        // successfully extracted one line, data is in op, x1, ..., x4, c.
+        parseLine(op, x1, x2, x3, x4, scene);
+    }
+    cout << scene.e.x << endl;
+    return scene;
+}
+
+static void parseLine(char op, float x1, float x2, float x3, float x4, sceneDesription scene)
+{
+    switch (op)
+    {
+    case 'e':
+        scene.e = parseEye(x1, x2, x3, x4);
+        break;
+    case 'a':
+        scene.light = parseAmbientLight(x1, x2, x3, x4);
+        break;
+    case 'd':
+        scene.lightDirs->push_back(parseLightDirection(x1, x2, x3, x4));
+        break;
+    case 'p':
+        scene.spotlightPositions->push_back(parseSpotlightPosition(x1, x2, x3, x4));
+        break;
+    case 'i':
+        scene.intensities->push_back(parseLightIntensity(x1, x2, x3, x4));
+        break;
+    case 'c':
+        scene.colors->push_back(parseColor(x1, x2, x3, x4));
+        break;
+    default:
+        if (x4 > 0)
+        {
+            scene.objects->push_back(parseSphere(op, x1, x2, x3, x4));
+        }
+        else
+        {
+            scene.objects->push_back(parsePlane(op, x1, x2, x3, x4));
+        }
+    }
+}
+
+eye parseEye(float x1, float x2, float x3, float x4)
+{
+    return eye{
+        .x = x1,
+        .y = x2,
+        .z = x3,
+        .additional = x4
+    };
+}
+
+ambientLight parseAmbientLight(float x1, float x2, float x3, float x4)
+{
+    return ambientLight{
+        .R = x1,
+        .G = x2,
+        .B = x3,
+        .A = x4
+    };
+}
+
+lightDir parseLightDirection(float x1, float x2, float x3, float x4)
+{
+    return lightDir{
+        .x = x1,
+        .y = x2,
+        .z = x3,
+        .isSpotlight = (bool) x4
+    };
+}
+
+spotlightPosition parseSpotlightPosition(float x1, float x2, float x3, float x4)
+{
+    return spotlightPosition{
+        .x = x1,
+        .y = x2,
+        .z = x3,
+        .w = x4
+    };
+}
+
+intensity parseLightIntensity(float x1, float x2, float x3, float x4)
+{
+    return intensity{
+        .R = x1,
+        .G = x2,
+        .B = x3,
+        .A = x4
+    };
+}
+
+color parseColor(float x1, float x2, float x3, float x4)
+{
+    return color{
+        .R = x1,
+        .G = x2,
+        .B = x3,
+        .A = x4
+    };
+}
+
+objectType getType(char c)
+{
+    switch (c)
+    {
+    case 'r':
+        return reflective;
+    case 't':
+        return transparent;
+    default:
+        return normal;
+    }
+}
+
+sphere parseSphere(char op, float x1, float x2, float x3, float x4)
+{
+    objectType type = getType(op);
+    return sphere{
+        .x = x1,
+        .y = x2,
+        .z = x3,
+        .radius = x4,
+        .type = type
+    };
+}
+
+plane parsePlane(char op, float x1, float x2, float x3, float x4)
+{
+    objectType type = getType(op);
+    return plane{
+        .a = x1,
+        .b = x2,
+        .c = x3,
+        .d = x4,
+        .type = type
+    };
+}
---
+++ Game/parser.h
@@ -0,0 +1,162 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <vector>
+
+using namespace std;
+
+enum objectType {normal, reflective, transparent};
+
+struct descriptor{};
+
+struct objectDescriptor : descriptor
+{
+    objectType type;
+};
+
+struct eye : descriptor
+{
+  eye(float x, float y, float z, float additional){
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->additional = additional;
+  };
+  eye();
+
+  float x;
+  float y;
+  float z;
+  float additional;
+};
+
+struct ambientLight : descriptor
+{
+  ambientLight(float R, float G, float B, float A){
+    this->R = R;
+    this->G = G;
+    this->B = B;
+    this->A = A;
+  };
+  ambientLight();
+  float R;
+  float G;
+  float B;
+  float A;
+};
+
+struct lightDir : descriptor
+{
+  lightDir(float x, float y, float z, bool isSpotlight){
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->isSpotlight = isSpotlight;
+  };
+  float x;
+  float y;
+  float z;
+  bool isSpotlight;
+};
+
+struct spotlightPosition : descriptor
+{
+  spotlightPosition(float x, float y, float z, float w){
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->w = w;
+  };
+  float x;
+  float y;
+  float z;
+  float w;
+};
+
+struct intensity : descriptor
+{
+  intensity(float R, float G, float B, float A){
+    this->R = R;
+    this->G = G;
+    this->B = B;
+    this->A = A;
+  };
+  float R;
+  float G;
+  float B;
+  float A;
+};
+
+struct sphere : objectDescriptor
+{
+  sphere(float x, float y, float z, float raduis, objectType type){
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->radius = radius;
+    this->type = type;
+  };
+  float x;
+  float y;
+  float z;
+  float radius;
+};
+
+struct plane : objectDescriptor
+{
+  plane(float a, float b, float c, float d, objectType type){
+    this->a = a;
+    this->b = b;
+    this->c = c;
+    this->d = d;
+    this->type = type;
+  };
+  float a;
+  float b;
+  float c;
+  float d;
+};
+
+struct color : descriptor
+{
+  color(float R, float G, float B, float A){
+    this->R = R;
+    this->G = G;
+    this->B = B;
+    this->A = A;
+  };
+  float R;
+  float G;
+  float B;
+  float A;
+};
+
+struct sceneDesription
+{
+  eye e;
+  ambientLight light;
+  vector<lightDir> *lightDirs;
+  vector<spotlightPosition> *spotlightPositions;
+  vector<intensity> *intensities;
+  vector<objectDescriptor> *objects;
+  vector<color> *colors;
+
+};
+
+
+
+
+static sceneDesription parseInputFile(string fileName);
+static void parseLine(char op, float x1, float x2, float x3, float x4, sceneDesription scene);
+eye parseEye(float x1, float x2, float x3, float x4);
+ambientLight parseAmbientLight(float x1, float x2, float x3, float x4);
+lightDir parseLightDirection(float x1, float x2, float x3, float x4);
+spotlightPosition parseSpotlightPosition(float x1, float x2, float x3, float x4);
+intensity parseLightIntensity(float x1, float x2, float x3, float x4);
+color parseColor(float x1, float x2, float x3, float x4);
+sphere parseSphere(char op, float x1, float x2, float x3, float x4);
+plane parsePlane(char op, float x1, float x2, float x3, float x4);
+
+objectType getType(char c);
+
+#endif
