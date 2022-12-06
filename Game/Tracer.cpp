d96f87d7c8b150493e696f706e3e4a7fd233ed70
alonz9911 <alons9911@gmail.com>
2022-12-06 02:05:07 +0200

    Fix light

--- Game/Tracer.cpp
+++ Game/Tracer.cpp
@@ -13,23 +13,34 @@ void Tracer::render() {
             glm::vec2 coordinate = {(float)x / (float)IMG_WIDTH, (float)y / (float)IMG_HEIGHT};
             coordinate = coordinate * 2.0f - 1.0f; // Converting 0 -> 1 to -1 -> 1
             glm::vec4 c = calcPixelColor(coordinate);
+            c = glm::clamp(c, glm::vec4(0.0f), glm::vec4(1.0f));
+            /*if (c.r > 0 || c.g > 0 || c.b > 0)
+            {
+                cout << c.r << ", " << c.g << ", " << c.b << ", " << c.a << endl;
+            }*/
+            
             imageData[4 * (x + y * IMG_WIDTH)] = (unsigned char) (c.r * 255.0f);
             imageData[4 * (x + y * IMG_WIDTH) + 1] = (unsigned char) (c.g * 255.0f);
             imageData[4 * (x + y * IMG_WIDTH) + 2] = (unsigned char) (c.b * 255.0f);
             imageData[4 * (x + y * IMG_WIDTH) + 3] = (unsigned char) (c.a * 255.0f);
+
+            if (c.r > 0 || c.g > 0 || c.b > 0)
+            {
+                //cout << (int)imageData[4 * (x + y * IMG_WIDTH)] << ", " << (int)imageData[4 * (x + y * IMG_WIDTH) + 1] << ", " << (int)imageData[4 * (x + y * IMG_WIDTH) + 2] << ", " << (int)imageData[4 * (x + y * IMG_WIDTH) +3] << endl;
+            }
 
         }
     }
 }
 
 glm::vec4 Tracer::calcPixelColor(glm::vec2 coor) {
-    glm::vec3 rayDirection = glm::vec3(coor.x, coor.y, 1.0f);
-    glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+    glm::vec3 rayDirection = glm::vec3(coor.x, coor.y, -1.0f);
+    glm::vec3 rayOrigin(0.0f, 0.0f, -1.0f);
     rayDirection = glm::normalize(rayDirection);
     float raduis = 0.5f;
     glm::vec3 sphereCenter(0.0f);
-    glm::vec3 sphereColor(240.0/255, 240.0/255, 15.0/255);
-    glm::vec3 lightDir(-1, -1, 1);
+    glm::vec3 sphereColor(0, 1, 1);
+    glm::vec3 lightDir(1, 1, 1);
     lightDir = glm::normalize(lightDir);
 
     // (bx^2 + by^2 + bz^2)t^2 + 2t(axbx + ayby + azbz) + (ax^2 + ay^2 + az^2 - r^2) = 0
@@ -54,7 +65,7 @@ glm::vec4 Tracer::calcPixelColor(glm::vec2 coor) {
             -b + sqrt(discriminant) / (2.0f * a),
             -b - sqrt(discriminant) / (2.0f * a)
         };*/
-        float t = max(-b + sqrt(discriminant) / (2.0f * a), -b - sqrt(discriminant) / (2.0f * a));
+        float t = (-b + glm::sqrt(discriminant)) / (2.0f * a);
 
         /*for (int i = 0; i < 2; i++)
         {
@@ -68,10 +79,10 @@ glm::vec4 Tracer::calcPixelColor(glm::vec2 coor) {
         glm::vec3 hitPosition = rayOrigin + rayDirection * t;
         glm::vec3 normal = hitPosition - sphereCenter;
         normal = glm::normalize(normal);
-
-        float intensity = max(glm::dot(normal, -lightDir), 0.0f);
+        float intensity = glm::max(glm::dot(normal, -lightDir), 0.0f);
+        cout << intensity << endl;
         return glm::vec4(intensity * sphereColor, 1.0f);
     }
-    return glm::vec4(15.0/255, 15.0/255, 240.0/255, 1.0f);
+    return glm::vec4(0, 0, 0, 1);
     
 }
